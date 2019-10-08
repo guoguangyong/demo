a00ee3bc72c9425c814c6900168848bb721cd61e
guoguangyong <ggy2440453666@163.com>
2019-10-08 20:29:45 +0800

    练习上传-发送短信自动化代码

--- page/add_message.py
+++ page/add_message.py
@@ -1,8 +1,9 @@
 # 新建信息
+
 from selenium.webdriver.common.by import By
 
 from base.base_action import BaseAction
-
+import allure
 
 class Add_Message(BaseAction):
 
@@ -15,11 +16,15 @@ class Add_Message(BaseAction):
     # 发送按钮
     message_recive_btn = By.ID,"com.android.mms:id/send_button_sms"
 
+    @allure.step(title="进行输入及发送操作")
     def input_message_reciver(self,text):
+        allure.attach("输入接收者的电话号码", text, allure.attach_type.TEXT)
         self.input(self.message_reciver,text)
 
     def input_message_text(self,text):
+        allure.attach("输入短信内容",text,allure.attach_type.TEXT)
         self.input(self.message_text,text)
 
     def click_message_recive_btn(self):
         self.click(self.message_recive_btn)
+        allure.attach("发送成功截图",self.driver.get_screenshot_as_png(),allure.attach_type.PNG)
--- scripts/test_add_del_message.py
+++ scripts/test_add_del_message.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from base.base_driver import init_driver
 from page.page import Page
 
@@ -14,8 +16,18 @@ class Test_Message_Option:
         time.sleep(5)
         self.driver.quit()
 
+    @pytest.allure.severity(pytest.allure.severity_level.NORMAL)
     def test_add_del_message(self):
         self.page.del_add_all_message().click_more_btn()
-        self.page.add_message().input_message_reciver(11111111111)
+        self.page.add_message().input_message_reciver("11111111111")
         self.page.add_message().input_message_text("您好！")
         self.page.add_message().click_message_recive_btn()
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_scripts001(self):
+        assert 0
+
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    def test_scripts002(self):
+        assert 0
+
